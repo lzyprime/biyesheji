29582a9923e5c723d4f1ee2e72adaeae6ab905e8
lzyprime <2383518170@qq.com>
2020-04-09 14:46:12 +0800

    debug 与 release 数据库

--- src/db/DB.kt
+++ src/db/DB.kt
@@ -3,20 +3,26 @@ package com.lzyprime.db
 import com.lzyprime.db.tables.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object DB {
-    private val db by lazy {
+    private val debugDB by lazy {
         Database.connect(
-            "jdbc:mariadb://127.0.0.1:3306/biyesheji",
-            "org.mariadb.jdbc.Driver",
-            "root",
-            "3821"
+            "jdbc:sqlite:/Users/prime/Desktop/biyesheji/server/src/db/ktor_server.sqlite",
+            "org.sqlite.JDBC"
         )
     }
 
-    operator fun invoke() = db
+    private val releaseDB by lazy {
+            Database.connect(
+                "jdbc:mariadb://127.0.0.1:3306/biyesheji",
+                "org.mariadb.jdbc.Driver",
+                "root",
+                "3821"
+            )
+    }
+
+    operator fun invoke() = debugDB
 
     @JvmStatic
     fun main(args: Array<String>) {
